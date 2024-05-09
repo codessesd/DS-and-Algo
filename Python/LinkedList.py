04c98da25d1e239d43f8b88c2d8632b425e9aae7
Rendz <rendani@obcode.co.za>
2024-05-09 12:45:33 +0200

    Function skeletons

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -1,6 +1,56 @@
 class Node:
-  def __init__(self, value, next) -> None:
+  def __init__(self, value) -> None:
     self.value = value
-    self.next = next
+    self.next = None
     
-class 
+class LinkedList:
+  def __init__(self, value) -> None:
+    node = Node(value)
+    self.head = node;
+    self.tail = node;
+    self.length = 1;
+    
+  def printList(self):
+    temp = self.head;
+    for i in range(self.length):
+      print(temp.value)
+      temp = temp.next
+      
+  def get():
+    pass
+  def set():
+    pass
+  def insert():
+    pass
+  
+  def append(self, value):
+    node = Node(value)
+    if self.length == 0:
+      self.head = node
+      self.tail = node
+    self.tail.next = node;
+    self.tail = node;
+    self.length += 1
+    
+      
+  def prepend(self,value):
+    pass
+  
+  def deleteLast():
+    pass
+  def deleteFist():
+    pass
+  def insert():
+    pass
+  def deleteNode():
+    pass
+  def reverse():
+    pass
+      
+    
+
+#Code test**************************************
+myLinkedList = LinkedList(10)
+myLinkedList.append(20)
+myLinkedList.append(30)
+myLinkedList.printList()
